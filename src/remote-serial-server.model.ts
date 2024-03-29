0f04872e9b3a4f0b89b484815d22429f9bc86b53
teddy1565 <teddy1565@gmail.com>
2024-03-30 06:49:31 +0800

    feat: 🎸 more implement for socket

--- src/remote-serial-server.model.ts
+++ src/remote-serial-server.model.ts
@@ -11,15 +11,71 @@ import { SocketServerSideEmitChannel,
 export abstract class AbsRemoteSerialServerSocket {
     protected abstract _socket: Socket;
 
+    /**
+     * Emits an event.
+     *
+     * @param channel Name of the channel
+     * @param message Values to send to listeners of this event
+     */
     abstract emit(channel: SocketServerSideEmitChannel, message: SocketServerSideEmitPayloadType): void;
+    /**
+     * Adds the `listener` function as an event listener for `channel`.
+     *
+     * @param channel Name of the channel
+     * @param listener Callback function
+     */
     abstract on(channel: SocketClientSideEmitChannel, listener: (data: SocketClientSideEmitPayloadType) => void): void;
+    /**
+     * Adds a one-time `listener` function as an event listener for `channel`.
+     *
+     * @param channel Name of the channel
+     * @param listener Callback function
+     */
     abstract once(channel: SocketClientSideEmitChannel, listener: (data: SocketClientSideEmitPayloadType) => void): void;
+    /**
+     * Disconnects this client.
+     *
+     * @example
+     * io.on("connection", (socket) => {
+     *   // disconnect this socket (the connection might be kept alive for other namespaces)
+     *   socket.disconnect();
+     *
+     *   // disconnect this socket and close the underlying connection
+     *   socket.disconnect(true);
+     * })
+     *
+     * @param {Boolean} close - if `true`, closes the underlying connection
+     * @return self
+     */
+    abstract disconnect(close?: boolean): void;
+
+    get id(): string {
+        return this._socket.id;
+    }
+
+    get connected(): boolean {
+        return this._socket.connected;
+    }
+
+    get disconnected(): boolean {
+        return this._socket.disconnected;
+    }
+
+    get nsp() {
+        return this._socket.nsp;
+    }
 }
 
 export abstract class AbsRemoteSerialServerSocketNamespace<T extends AbsRemoteSerialServerSocket> {
     protected abstract _namespace: Namespace;
 
-    abstract on(channel: SocketIONamespaceOnEvent, listener: (socket: T) => void): void;
+    /**
+     * Adds the `listener` function as an event listener for `ev`.
+     *
+     * @param ev Name of the ev
+     * @param listener Callback function
+     */
+    abstract on(ev: SocketIONamespaceOnEvent, listener: (socket: T) => void): void;
 }
 
 
